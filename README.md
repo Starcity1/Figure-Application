046b068c5dd25bf27e5124782fde9e2085d82bde
David Rodriguez <david.rodriguez220443@gmail.com>
2023-04-12 21:45:24 -0500

    Uploaded all already-generated files to github.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Figure-Application
+This is a prototype version of an application that will generate figures for
+LMA data.
---
+++ main.py
@@ -0,0 +1,16 @@
+# Figure Application
+# Author : David Rodriguez Sanchez
+# Version : 0.0.1
+# Please, refer to the README.md found on the github repository which contains all the information
+# for this application.
+#
+#
+import tkinter as tk
+
+
+def main():
+    pass
+
+
+if "__name__" == __main__:
+    main()
