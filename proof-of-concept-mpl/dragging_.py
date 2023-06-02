b4df302e7cf8f8eddde1869d95940eaa3c7bca7a
David Rodriguez <david.rodriguez220443@gmail.com>
2023-06-02 16:16:33 -0500

    Added dragging.py to showcase the dragging of widgets.

---
+++ proof-of-concept-mpl/dragging_.py
@@ -0,0 +1,17 @@
+from tkinter import *
+window = Tk()
+window.state('zoomed')
+window.configure(bg = 'green4')
+
+def drag(event):
+    event.widget.place(x=event.x_root, y=event.y_root,anchor=CENTER)
+
+card = Canvas(window, width=74, height=97, bg='blue')
+card.place(x=300, y=600,anchor=CENTER)
+card.bind("<B1-Motion>", drag)
+
+another_card = Canvas(window, width=74, height=97, bg='red')
+another_card.place(x=600, y=600,anchor=CENTER)
+another_card.bind("<B1-Motion>", drag)
+
+window.mainloop()
